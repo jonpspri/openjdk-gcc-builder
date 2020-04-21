57b3a07f55f257ec9c8f0b8fca5c425378d448aa
Jonathan Springer <jonpspri@gmail.com>
2020-04-21 13:58:19 -0400

    Add gcc 9.1.0 to the shasums and set as default build.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-gcc_version=${GCC_VERSION:-8.2.0}
+gcc_version=${GCC_VERSION:-9.1.0}
 target_registry=${ADOPTOPENJDK_TARGET_REGISTRY:-adoptopenjdk}
 
 docker build --build-arg GCC_VERSION="$gcc_version" \
